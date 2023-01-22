c1ceffa3dd9a16c28420255f805ea363e595e03f
Hexy32 <sunshine.goody8762@gmail.com>
2023-01-22 18:47:45 -0500

    Now using Strict Mode for React

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import './styles/index.css'
 
 import App from './App'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)

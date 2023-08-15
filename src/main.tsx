a06b60ffcf4558ce82133ff932c044d8c0dc04f9
Hexy32 <sunshine.goody8762@gmail.com>
2023-08-14 23:52:59 -0400

    Added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import './styles/index.css'
 
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -7,5 +8,6 @@ import ReactDOM from 'react-dom/client'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 )
